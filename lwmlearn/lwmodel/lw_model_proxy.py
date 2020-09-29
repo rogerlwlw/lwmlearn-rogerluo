4ca5fd87211e35e71324ef65b4b92d7740a90828
rogerluo <coolww@outlook.com>
2020-09-29 15:09:23 +0800

    change docs

--- lwmlearn/lwmodel/lw_model_proxy.py
+++ lwmlearn/lwmodel/lw_model_proxy.py
@@ -1,73 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 Class :class:`~.LW_model` provides common methods for ML proccess such as model 
-evaluation and feature selecting
-
-use example
-------------
-
-the following runs a ``clean_oht_frf_OneSidedSelection_XGBClassifier`` pipeline
-on fake classification data. The pipe line performs ``data cleanning`` --> 
-``one hot encoding`` --> ``randomforest feature selection`` --> ``OneSidedSelection`` 
-on training dataset and finally use a XGBClassifier to train processed data. 
-Then the fitted pipeline could be used to make predictions on test data. 
-
-.. ipython::
-    :okwarning:
-    
-    @supress    
-    In [1]: import warnings
-       ...: warnings.filterwarnings("ignore")
-       
-    In [2]: from lwmlearn import LW_model
-       ...: from sklearn.datasets import make_classification
-       ...: from sklearn.model_selection import train_test_split
-    
-    In [4]: X, y = make_classification(10000, n_redundant=20, n_features=50, flip_y=0.1)
-
-    In [5]: X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
-    
-    In [6]: m = LW_model('clean_oht_frf_OneSidedSelection_XGBClassifier', verbose=1)
-    
-    In [7]: m.fit(X_train, y_train)
-    
-    In [9]: m.predict(X_test)   
-    
-    In [11]: m.test_score(X_test, y_test, cv=1, scoring=['KS', 'roc_auc'])
-    
-    In [12]: m.cv_validate(X_train, y_train, scoring=['roc_auc', 'KS'])
-
-    # auto tuning parameters by bayesian search and update model
-    In [13]: m.opt_sequential((X, y), kind='bayesiancv')
-
-.. ipython::
-    :okwarning:
-        
-    # plot search learning curve
-    @savefig plot_learning_curve.png
-    In [14]: m.plot_gridcv(m.kws_attr['gridcvtab'][0]) 
-    
-.. ipython::
-    :okwarning:
-        
-    # plot lift curve for trainset
-    @savefig plot_lift_train.png
-    In [46]: m.plot_lift(X_train, y_train, max_leaf_nodes=10)
-
-.. ipython::
-    :okwarning:
-        
-    # plot lift curve for test set with bins cut by trainset
-    @savefig plot_lift_test.png
-    In [45]: m.plot_lift(X_test, y_test, use_self_bins=True)
-
-.. ipython::
-    :okwarning:
-        
-    # plot two plots together
-    @savefig plot_auclift.png
-    In [47]: m.plot_AucLift(X_test, y_test, fit_train=False)
-    
+evaluation and feature/model selecting
    
 Created on Wed Dec 11 18:39:56 2019
 
